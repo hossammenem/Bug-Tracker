98e46617c299f3c72556dada05df5727cff9258f
hossammenem <88946630+hossammenem@users.noreply.github.com>
2023-05-01 19:43:38 +0300

    Update Header.tsx

--- client/src/views/Header.tsx
+++ client/src/views/Header.tsx
@@ -2,6 +2,7 @@ import HeaderLinks from "../auth0/headerLinks";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 import getUsername from "../api/getUsername";
+import PreLoader from "./PreLoader";
 
 function Header() {
     const { user, isAuthenticated } = useAuth0();
